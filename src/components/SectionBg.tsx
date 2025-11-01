889142d44d9f88085a88ac45ca3271e3fce02839
Disura Aberathna <disura2005@gmail.com>
2025-11-01 12:42:39 +0530

    Update SectionBg.tsx

--- src/components/SectionBg.tsx
+++ src/components/SectionBg.tsx
@@ -1,8 +1,8 @@
 const SectionBg = () => (
   <div className="section-bg-layer">
     <div className="absolute inset-0 bg-grid opacity-40" />
-    <div className="absolute -top-24 -left-24 w-80 h-80 rounded-full bg-blob-yellow" />
-    <div className="absolute -bottom-24 -right-24 w-96 h-96 rounded-full bg-blob-yellow" />
+    {/*<div className="absolute -top-24 -left-24 w-80 h-80 rounded-full bg-blob-yellow" />*/}
+    {/*<div className="absolute -bottom-24 -right-24 w-96 h-96 rounded-full bg-blob-yellow" />*/}
   </div>
 );
 

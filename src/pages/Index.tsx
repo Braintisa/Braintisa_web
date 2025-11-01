378689d660505e5125c103fcd657028455c51941
Disura Aberathna <disura2005@gmail.com>
2025-11-01 12:42:54 +0530

    add pre loader

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -8,36 +8,68 @@ import Testimonials from "@/components/Testimonials";
 import Contact from "@/components/Contact";
 import Footer from "@/components/Footer";
 import {Element} from "react-scroll";
+import {AnimatePresence} from "framer-motion";
+import Preloader from "@/components/Preloader.tsx";
+import {useEffect, useState} from "react";
 
 const Index = () => {
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        const handleLoaded = () => {
+            setTimeout(() => {
+                setIsLoading(false);
+            }, 1000);
+        }
+
+        if (document.readyState === "complete") {
+            setIsLoading(false);
+        } else {
+            addEventListener("load", handleLoaded);
+        }
+
+        return () => {
+            document.removeEventListener("load", handleLoaded);
+        }
+    }, []);
+
     return (
         <div className="min-h-screen">
-            <Navbar/>
-            <main>
-                <Element name="hero">
-                    <Hero/>
-                </Element>
-                <Element name="about">
-                    <About/>
-                </Element>
-                <Element name="services">
-                    <Services/>
-                </Element>
-                <Element name="projects">
-                    <Projects/>
-                </Element>
-                <Element name="team">
-                    <Team/>
-                </Element>
-                <Element name="testimonials">
-                    <Testimonials/>
-                </Element>
-                <Element name="contact">
-                    <Contact/>
-                </Element>
-            </main>
-            <Footer/>
+            <AnimatePresence mode="wait">
+                {isLoading && <Preloader/>}
+            </AnimatePresence>
+
+            {!isLoading &&
+                <>
+                    <Navbar/>
+                    <main>
+                        <Element name="hero">
+                            <Hero/>
+                        </Element>
+                        <Element name="about">
+                            <About/>
+                        </Element>
+                        <Element name="services">
+                            <Services/>
+                        </Element>
+                        <Element name="projects">
+                            <Projects/>
+                        </Element>
+                        <Element name="team">
+                            <Team/>
+                        </Element>
+                        <Element name="testimonials">
+                            <Testimonials/>
+                        </Element>
+                        <Element name="contact">
+                            <Contact/>
+                        </Element>
+                    </main>
+                    <Footer/>
+                </>
+            }
         </div>
+
     );
 };
 
